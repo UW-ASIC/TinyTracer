78bb0a6f2eab67e98ac4d2763a9891a4cd6f1cbe
raphaelkuperfarb <raphaelkuperfarb@gmail.com>
2026-09-24 14:15:16 -0400

    rtl for clkdiv

--- src/io/clkdiv.sv
+++ src/io/clkdiv.sv
@@ -9,4 +9,17 @@ module clkdiv (
     output logic       q
 );
 
+reg [ACCW-1:0] sr;
+
+always @(posedge clk) begin
+    if (!rst_n) begin
+        sr <= {ACCW{1'b0}}; 
+        q <= 0;
+    end else if (sr[ACCW-1]) begin
+        sr <= sr + b;
+        q <= ~q;
+    end else sr <= sr - c;
+end
+
+
 endmodule
